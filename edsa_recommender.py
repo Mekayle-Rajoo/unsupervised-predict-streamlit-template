0147206e9a90bbe54a9bbf3b2cbe95e046acae59
Mekayle Rajoo <mekayle@gmail.com>
2022-07-14 13:20:29 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -31,6 +31,9 @@ import streamlit as st
 # Data handling dependencies
 import pandas as pd
 import numpy as np
+import random
+import re
+from PIL import Image
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -45,57 +48,84 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    
+    # Creates a main title and subheader on your page -
+    # these are static across all pages
+    
+    st.image("resources/logogif.gif")
+    
+   
+    
+    
+    page_options = ["HOME","Solution Overview", "FAQ", "Insights","Download our app", "Contact Us"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    if page_selection == "Recommender System":
+    
+    if page_selection == "FAQ":
+            st.write("here are some questions")
+            select = st.selectbox("FAQ",("What is Netflix?","How much does it cost?"))
+            
+            if select == "What is Netflix?":
+                st.write("Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!")
+            if select == "How much does it cost?":
+                st.write("Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from R49 to R199 a month. No extra costs, no contracts.")
+    
+    if page_selection == "HOME":
         # Header contents
-        st.write('# Movie Recommender Engine')
-        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
-        st.image('resources/imgs/Image_header.png',use_column_width=True)
-        # Recommender System algorithm selection
-        sys = st.radio("Select an algorithm",
-                       ('Content Based Filtering',
-                        'Collaborative Based Filtering'))
+        st.image("resources/moviesgif.gif")
+        
+        
+        genre = st.radio( "New to Lumiere? Sign up or login in if you have an account",('login', 'sign up'))
+        
+        if genre == "sign up":
+            email = st.text_input('Ready to watch? Enter your email to start a membership', '')
+            
+            if st.button("next"):
+            
+                if (re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',email)):
+                    st.write('Welcome to Lumiere', email.split("@")[0].capitalize(),","," looks like you are new here.\n To customize your\
+                    experience please select 3 movies which you enjoy.")
 
-        # User-based preferences
-        st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
-        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
-        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
-        fav_movies = [movie_1,movie_2,movie_3]
+                    movie_1 = st.selectbox('First Option',title_list[14930:15200])
+                    movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+                    movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+                    fav_movies = [movie_1,movie_2,movie_3]
 
-        # Perform top-10 movie recommendation generation
-        if sys == 'Content Based Filtering':
-            if st.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = content_model(movie_list=fav_movies,
-                                                            top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-                except:
-                    st.error("Oops! Looks like this algorithm does't work.\
-                              We'll need to fix it!")
+                    # Perform top-10 movie recommendation generation
 
 
-        if sys == 'Collaborative Based Filtering':
-            if st.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = collab_model(movie_list=fav_movies,
-                                                           top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-                except:
-                    st.error("Oops! Looks like this algorithm does't work.\
-                              We'll need to fix it!")
+                    if st.button("Recommend"):
+                        top_recommendations = content_model(movie_list=fav_movies,top_n=10)
+                        st.title("We think you'll like:")
+                        for i,j in enumerate(top_recommendations):
+                            st.subheader(str(i+1)+'. '+j)
+                            
+                else:
+                    st.write("Please enter a valid email")
+                    
+            
+                
+        if genre == "login":
+            
+            title = st.text_input('email address', '')
+            password = st.text_input('password', '', type="password")
+      
+         
+            if st.button("Login"):
+            
+                if (re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',title)) and password != "":
+                
+                    st.write('Welcome back to Explore!', title.split("@")[0].capitalize(),"userId ","(",random.randint(0, 10000038),")","here are some titles we think you may enjoy")
+                    
+                else:
+                    st.write("Please enter valid login details")
+        
 
+        
+                
 
     # -------------------------------------------------------------------
 
